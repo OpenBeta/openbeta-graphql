ae3b2014dc08ea76e68d893511df99962b111c44
viet nguyen <vietnguyen@noreply>
2021-12-28 12:22:02 -0600

    Missing new files

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from 'apollo-server'
+import { connection } from 'mongoose'
+import { schema as graphQLSchema } from './schema/GraphQLSchema'
+import Climbs from './model/Climbs'
+import { connectDB } from './db'
+import Areas from './model/Areas'
+
+// eslint-disable-next-line
+(async function (): Promise<void> {
+  const server = new ApolloServer({
+    introspection: true,
+    schema: graphQLSchema,
+    dataSources: () => {
+      return {
+        climbs: new Climbs(connection.db.collection('Climb')),
+        areas: new Areas(connection.db.collection('areas'))
+      }
+    }
+  })
+
+  await connectDB()
+  await server.listen().then((): void => {
+    console.log('🚀 Server ready!')
+  })
+})()
---
+++ src/schema/GraphQLSchema.ts
@@ -0,0 +1,46 @@
+import { makeExecutableSchema } from '@graphql-tools/schema'
+
+import { typeDef as Climb } from './ClimbTypeDef'
+import { typeDef as Area } from './AreaTypeDef'
+
+const resolvers = {
+  Query: {
+    climb: async (parent, { ID }, { dataSources }) => {
+      return dataSources.climbs.findOneById(ID)
+    },
+
+    areas: async (
+      _,
+      { name, nameContains }: { name: string, nameContains: string },
+      { dataSources: { areas } }
+    ) => {
+      if (name !== '') return areas.findByName(name)
+      if (nameContains !== '') return areas.findByName(nameContains, true)
+
+      return areas.all()
+    },
+
+    area: async (_, { id }: { id: string }, { dataSources: { areas } }) => {
+      if (id !== '') return areas.findOneById(id)
+      return null
+    }
+  },
+
+  Climb: {
+    // TODO: let's see if we need to create any field resolvers
+  },
+
+  Area: {
+    children: async (parent, _, { dataSources: { areas } }) => {
+      if (parent.children.length > 0) {
+        return areas.findManyByIds(parent.children)
+      }
+      return null
+    }
+  }
+}
+
+export const schema = makeExecutableSchema({
+  typeDefs: [Climb, Area],
+  resolvers
+})
