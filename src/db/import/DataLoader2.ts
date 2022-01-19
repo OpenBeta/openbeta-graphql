53973f0c59854caf39188483c5927803220888fa
viet nguyen <vietnguyen@noreply>
2022-01-19 10:08:15 -0800

    WIP

--- src/db/import/DataLoader2.ts
+++ src/db/import/DataLoader2.ts
@@ -7,6 +7,8 @@ import { linkAreas } from './LinkParent.js'
 import { createClimbModel } from '../ClimbSchema.js'
 import { ClimbType } from '../ClimbTypes.js'
 import transformClimbRecord from './ClimbTransformer.js'
+import transformAreaRecord from './AreaTransformer.js'
+
 
 import readline from 'node:readline'
 import fs from 'node:fs'
@@ -24,69 +26,23 @@ const main = async (): Promise<void> => {
   await _dropCollection(tmpAreas)
   await _dropCollection(tmpClimbs)
 
-  // const areaModel: mongoose.Model<AreaType> = createAreaModel(tmpAreas)
+  const areaModel: mongoose.Model<AreaType> = createAreaModel(tmpAreas)
   const climbModel: mongoose.Model<ClimbType> = createClimbModel(tmpClimbs)
-  // let areaCount = 0
-  let climbCount = 0
 
-  const chunkSize = 100
-  let chunk: ClimbType[] = []
-
-  const rl = readline.createInterface({
-    input: fs.createReadStream(contentDir + '/climbs/ca-routes.jsonlines'),
-    terminal: false
-  })
-
-  for await (const line of rl) {
-    climbCount = climbCount + 1
-    const climb = transformClimbRecord(JSON.parse(line))
-    chunk.push(climb)
-    if (chunk.length >= chunkSize) {
-      await climbModel.insertMany(chunk, { ordered: false })
-      chunk = []
-    }
-  }
-
-  if (chunk.length > 0) {
-    await climbModel.insertMany(chunk, { ordered: false })
-  }
-
-  /* eslint-disable-next-line */
-  // stream.on('line', async (line: string): Promise<void> => {
-  //   const climb = transformClimbRecord(JSON.parse(line))
-  //   chunk.push(climb)
-  //   climbCount = climbCount + 1
-  //   console.log('chunk', chunk.length)
-  //   // await climbModel.insertMany(climb, { ordered: false })
-  //   if (chunk.length >= chunkSize) {
-  //     console.log('Inserting', climbCount)
-  //     stream.pause()
-  //     await climbModel.insertMany(chunk, { ordered: false })
-  //     chunk = []
-  //     stream.resume()
-  //   }
-  // })
-
-  // /* eslint-disable-next-line */
-  // stream.on('end', async () => {
-  //   console.log('end', chunk)
-  //   if (chunk.length > 0) {
-  //     stream.pause()
-  //     console.log('Inserting ', chunk.length)
-  //     await climbModel.insertMany(chunk, { ordered: false })
-  //     stream.resume()
-  //   }
-  // })
+  const foos = await Promise.all([
+    load<ClimbType>(contentDir + '/climbs/or-routes.jsonlines', transformClimbRecord, climbModel),
+    load<AreaType>(contentDir + '/climbs/or-areas.jsonlines', transformAreaRecord, areaModel),
+  ])
 
   console.log('Content basedir: ', contentDir)
   // console.log('Areas Loaded ', areaCount)
-  console.log('Climbs Loaded ', climbCount)
+  console.log('Climbs Loaded ', foos)
 
   // await linkAreas(tmpAreas)
   console.log('Areas linked')
   console.log('Dropping old collections...')
-  // await _dropCollection('areas')
-  // await mongoose.connection.db.renameCollection(tmpAreas, 'areas')
+  await _dropCollection('areas')
+  await mongoose.connection.db.renameCollection(tmpAreas, 'areas')
   await _dropCollection('climbs')
   await mongoose.connection.db.renameCollection(tmpClimbs, 'climbs')
   console.log('Done.')
@@ -99,16 +55,30 @@ const _dropCollection = async (name: string): Promise<void> => {
   } catch (e) { }
 }
 
-connectDB(main)
+const load = async<T extends ClimbType|AreaType>(fileName: string, transformer: (row: any) => T, model: mongoose.Model<T>): Promise<number> => {
+  let count = 0
+  const chunkSize = 100
+  let chunk: T[] = []
 
-// const readline = require('readline');
-// const fs = require('node:fs')
-// //var x = []
-// readline.createInterface({
-//     input: fs.createReadStream('./climbs/ca-routes.jsonlines'),
-//     terminal: false
-// }).on('line', function(line) {
-//     const climb = JSON.parse(line);
-//     //x.push(log['id']);
-//     console.log(climb['route_name'])
-// });
+  const rl = readline.createInterface({
+    input: fs.createReadStream(fileName),
+    terminal: false
+  })
+
+  for await (const line of rl) {
+    count = count + 1
+    const record = transformer(JSON.parse(line))
+    chunk.push(record)
+    if (chunk.length >= chunkSize) {
+      await model.insertMany(chunk, { ordered: false })
+      chunk = []
+    }
+  }
+
+  if (chunk.length > 0) {
+    await model.insertMany(chunk, { ordered: false })
+  }
+  return count
+}
+
+connectDB(main)
