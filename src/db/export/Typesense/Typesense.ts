c8922ecd243bc8663b932a2d4fe4d15d444fa6b0
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-02-27 11:41:18 +0100

    DB exporter script to Typesense (#55)
    * Initial bulk inserts
    * Move Typesense code to its own dir
    * Convert disciplines to string

---
+++ src/db/export/Typesense/Typesense.ts
@@ -0,0 +1,153 @@
+import Typesense from 'typesense'
+import { connectDB, gracefulExit, createAreaModel } from '../../index.js'
+import { disciplinesToEnums } from './Utils.js'
+const chunkSize = 5000
+
+const schema = {
+  name: 'climbs',
+  num_documents: 0,
+  fields: [
+    {
+      name: 'climbId',
+      type: 'string' as const,
+      index: false,
+      optional: true
+    },
+    {
+      name: 'climb_name',
+      type: 'string' as const,
+      facet: false
+    },
+    {
+      name: 'climb_desc',
+      type: 'string' as const,
+      facet: false
+    },
+    {
+      name: 'fa',
+      type: 'string' as const,
+      facet: false
+    },
+    {
+      name: 'disciplines',
+      type: 'string' as const,
+      facet: true
+    }
+    // {
+    //   name: 'typeSport',
+    //   type: 'bool' as const,
+    //   facet: true
+    // },
+    // {
+    //   name: 'typeTrad',
+    //   type: 'bool' as const,
+    //   facet: true
+    // },
+    // {
+    //   name: 'typeBouldering',
+    //   type: 'bool' as const,
+    //   facet: true
+    // },
+    // {
+    //   name: 'typeAlpine',
+    //   type: 'bool' as const,
+    //   facet: true
+    // },
+    // {
+    //   name: 'typeMixed',
+    //   type: 'bool' as const,
+    //   facet: true
+    // },
+    // {
+    //   name: 'typeAid',
+    //   type: 'bool' as const,
+    //   facet: true
+    // },
+    // {
+    //   name: 'typeTR',
+    //   type: 'bool' as const,
+    //   facet: true
+    // }
+  ]
+//   default_sorting_field: 'climb_name'
+}
+
+const onDBConnected = async (): Promise<void> => {
+  const node = process.env.TYPESENSE_NODE ?? ''
+  const apiKey = process.env.TYPESENSE_API_KEY ?? ''
+
+  if (node === '' || apiKey === '') {
+    gracefulExit(1)
+  }
+
+  console.log('Start pushing data to TypeSense')
+
+  const typesense = new Typesense.Client({
+    nodes: [
+      {
+        host: node,
+        port: 443,
+        protocol: 'https'
+      }
+    ],
+    apiKey,
+    numRetries: 3, // A total of 4 tries (1 original try + 3 retries)
+    connectionTimeoutSeconds: 120, // Set a longer timeout for large imports
+    logLevel: 'info'
+  })
+
+  try {
+    // Delete if the collection already exists from a previous example run
+    await typesense.collections('climbs').delete()
+  } catch (error) {
+    console.log(error)
+    // do nothing
+  }
+
+  try {
+    await typesense.collections().create(schema)
+  } catch (error) {
+    console.log(error)
+  }
+
+  const areaModel = createAreaModel()
+  const agg = areaModel.aggregate([{ $match: { 'metadata.leaf': true } }]).unwind('climbs').replaceRoot('climbs')
+
+  let count: number = 0
+  let chunks: any[] = []
+
+  for await (const doc of agg) {
+    if (chunks.length < chunkSize) {
+      doc.id = doc._id.toString()
+      chunks.push({
+        climbId: doc.id,
+        climb_name: doc.name,
+        climb_desc: doc.description ?? '',
+        fa: doc.fa ?? '',
+        disciplines: disciplinesToEnums(doc.type)
+      })
+    } else {
+      count = count + chunkSize
+      console.time(`Pushing batch: ${count / chunkSize}`)
+      try {
+        await typesense.collections('climbs').documents().import(chunks)
+      } catch (e) {
+        console.log(e)
+      }
+      console.timeEnd(`Pushing batch: ${count / chunkSize}`)
+      chunks = []
+    }
+  }
+
+  if (chunks.length > 0) {
+    // push remaining
+    count = count + chunks.length
+    await typesense.collections('climbs').documents().import(chunks)
+    // await index.saveObjects(chunks, { autoGenerateObjectIDIfNotExist: true })
+  }
+
+  console.log('Record uploaded: ', count)
+  gracefulExit()
+}
+
+connectDB(onDBConnected)
