6fa576a2fdf23adbd3709596de53bafbccad39cc
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2022-07-01 15:24:04 +0200

    New query to get tags by users (#84)

--- src/graphql/media/queries.ts
+++ src/graphql/media/queries.ts
@@ -1,5 +1,6 @@
 import muid from 'uuid-mongodb'
 import { getMediaModel } from '../../db/index.js'
+import { MediaListByAuthorType } from '../../db/MediaTypes.js'
 
 const MediaQueries = {
 
@@ -12,6 +13,47 @@ const MediaQueries = {
       return rs
     }
     return []
+  },
+
+  getRecentTags: async (_, { userLimit = 10 }: {userLimit: number | undefined}): Promise<MediaListByAuthorType[]> => {
+    const rs = await getMediaModel().aggregate<MediaListByAuthorType>([
+      {
+        $project: {
+          mediaUuid: 1,
+          mediaUrl: 1,
+          mediaType: 1,
+          destinationId: 1,
+          destType: 1,
+          onModel: 1,
+          authorUuid: { $substr: ['$mediaUrl', 3, 36] }
+        }
+      },
+      {
+        $group: {
+          _id: '$authorUuid',
+          mediaList: { $push: '$$ROOT' }
+        }
+      },
+      {
+        $limit: userLimit // limit the number of authors
+      },
+      {
+        $unwind: '$mediaList'
+      },
+      {
+        $unset: 'mediaList.authorUuid'
+      },
+      {
+        $sort: { 'mediaList._id': -1 }
+      },
+      {
+        $group: {
+          _id: '$_id',
+          tagList: { $push: '$mediaList' }
+        }
+      }
+    ])
+    return rs
   }
 }
 
