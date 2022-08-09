0c8765e8f804a7bd6f199477eeba4d613af47522
Downster <b.downing.code@gmail.com>
2022-08-09 10:02:58 -0700

    Changes

--- src/db/index.ts
+++ src/db/index.ts
@@ -28,6 +28,7 @@ export const connectDB = async (onConnected: () => any = defaultFn): Promise<any
   const user = checkVar('MONGO_INITDB_ROOT_USERNAME')
   const pass = checkVar('MONGO_INITDB_ROOT_PASSWORD')
   const server = checkVar('MONGO_SERVICE')
+  const rsName = checkVar('MONGO_REPLICA_SET_NAME')
 
   logger.info(
     `Connecting to database 'mongodb://${user}:****@${server}'...`
@@ -43,7 +44,7 @@ export const connectDB = async (onConnected: () => any = defaultFn): Promise<any
       }
     )
     await mongoose.connect(
-      `mongodb://${user}:${pass}@${server}:27017/opentacos?authSource=admin&readPreference=primary&ssl=false&replicaSet=rs0`,
+      `mongodb://${user}:${pass}@${server}/opentacos?authSource=admin&readPreference=primary&ssl=false&replicaSet=${rsName}`,
       { autoIndex: false }
     )
   } catch (e) {
@@ -57,7 +58,6 @@ export const createIndexes = async (): Promise<void> => {
   await getAreaModel().ensureIndexes()
   await getMediaModel().ensureIndexes()
   await getTickModel().ensureIndexes()
-
 }
 
 export const gracefulExit = async (exitCode: number = 0): Promise<void> => {
