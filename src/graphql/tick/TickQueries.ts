a25b5e48ec583401ba19b70d97b35044c730710e
Downster <b.downing.code@gmail.com>
2022-08-25 16:39:35 -0700

    Code linted

--- src/graphql/tick/TickQueries.ts
+++ src/graphql/tick/TickQueries.ts
@@ -1,17 +1,17 @@
-import { TickType } from "../../db/TickTypes";
-import type TickDataSource from "../../model/TickDataSource";
+import { TickType } from '../../db/TickTypes'
+import type TickDataSource from '../../model/TickDataSource'
 
 const TickQueries = {
-    userTicks: async (_, input, { dataSources }): Promise<TickType[] | null> => {
-        const { ticks }: { ticks: TickDataSource } = dataSources
-        const { userId } = input
-        return await ticks.ticksByUser(userId)
-    },
-    userTicksByClimbId: async (_, input, { dataSources }): Promise<TickType[] | null> => {
-        const { ticks }: { ticks: TickDataSource } = dataSources
-        const { climbId, userId } = input
-        return await ticks.ticksByUserAndClimb(userId, climbId)
-    }
+  userTicks: async (_, input, { dataSources }): Promise<TickType[] | null> => {
+    const { ticks }: { ticks: TickDataSource } = dataSources
+    const { userId } = input
+    return await ticks.ticksByUser(userId)
+  },
+  userTicksByClimbId: async (_, input, { dataSources }): Promise<TickType[] | null> => {
+    const { ticks }: { ticks: TickDataSource } = dataSources
+    const { climbId, userId } = input
+    return await ticks.ticksByUserAndClimb(userId, climbId)
+  }
 }
 
 export default TickQueries
