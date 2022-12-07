d178c052482fd63beadd988f152f823539cb2afb
viet nguyen <vietnguyen@noreply>
2022-12-07 16:50:49 +0100

    Add a delay

--- src/model/__tests__/updateAreas.ts
+++ src/model/__tests__/updateAreas.ts
@@ -19,7 +19,7 @@ describe('Areas', () => {
       await getClimbModel().collection.drop()
       await createIndexes()
     } catch (e) {
-      console.log('Cleaning up db before test')
+      console.log('Cleaning up db before test', e)
     }
     areas = createInstance()
   })
@@ -185,29 +185,33 @@ describe('Areas', () => {
 
   it('should not create duplicate countries', async () => {
     await areas.addCountry('ita')
-    await expect(areas.addCountry('ita')).rejects.toThrow('E11000 duplicate key error')
+
+    // eslint-disable-next-line
+    await new Promise(res => setTimeout(res, 2000))
+
+    await expect(areas.addCountry('ita')).rejects.toThrowError('E11000 duplicate key error')
   })
 
   it('should not create duplicate sub-areas', async () => {
     const fr = await areas.addCountry('fra')
     await areas.addArea(testUser, 'Verdon Gorge', fr.metadata.area_id)
     await expect(areas.addArea(testUser, 'Verdon Gorge', fr.metadata.area_id))
-      .rejects.toThrow('E11000 duplicate key error')
+      .rejects.toThrowError('E11000 duplicate key error')
   })
 
   it('should fail when calling without a parent country', async () => {
     await expect(areas.addArea(testUser, 'Peak District ', null, 'GB'))
-      .rejects.toThrow()
+      .rejects.toThrowError()
   })
 
   it('should fail when calling with a non-existent parent id', async () => {
     const notInDb = muuid.from('abf6cb8b-8461-45c3-b46b-5997444be867')
     await expect(areas.addArea(testUser, 'Land\'s End ', notInDb))
-      .rejects.toThrow()
+      .rejects.toThrowError()
   })
 
   it('should fail when calling with null parents', async () => {
     await expect(areas.addArea(testUser, 'Land\'s End ', null, '1q1'))
-      .rejects.toThrow()
+      .rejects.toThrowError()
   })
 })
