164043ef3433005434597dee4951779f267461e8
CocoisBuggy <g.colinharley@gmail.com>
2022-12-08 13:50:20 +0200

    📜️ Adding new documentation for Types and Model
    This is important, and the documentation is passed effectively out of
    the type and into model consumers.
    
    ⚠️ Committed without verify

--- src/db/TickSchema.ts
+++ src/db/TickSchema.ts
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose'
-import { TickType } from './TickTypes'
+import { TickSource, TickType } from './TickTypes'
 
 const { Schema } = mongoose
 
+/**
+ * Tick Schema
+ *
+ * The tick schema defines how ticks are stored and serialized in the mongo database.
+ * see the TickTypes.ts file for the typescript interface that defines types as they
+ * are used within the application. Getting documents from this schema should kick out
+ * TickType objects.
+ */
 export const TickSchema = new Schema<TickType>({
   name: { type: Schema.Types.String, required: true, index: true },
   notes: { type: Schema.Types.String, required: false },
@@ -12,7 +20,8 @@ export const TickSchema = new Schema<TickType>({
   attemptType: { type: Schema.Types.String, required: true, index: true, default: '' },
   dateClimbed: { type: Schema.Types.String, required: true },
   grade: { type: Schema.Types.String, required: true, index: true },
-  source: { type: Schema.Types.String, enum: ['MP', 'OB'], required: true, index: true }
+  // Bear in mind that these enum types must be kept in sync with the TickSource enum
+  source: { type: Schema.Types.String, enum: ['MP', 'OB'] as TickSource[], required: true, index: true }
 })
 
 TickSchema.index({ climbId: 1, dateClimbed: 1, style: 1, userId: 1, source: 1 }, { unique: true })
