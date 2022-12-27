c2e7179cbd5d2fd38b57a8d451a43d51e7394e31
viet nguyen <vietnguyen@noreply>
2022-12-27 19:12:51 +0100

    Fix mutation returned values

--- src/graphql/climb/ClimbMutations.ts
+++ src/graphql/climb/ClimbMutations.ts
@@ -1,33 +1,31 @@
-import muid, { MUUID } from 'uuid-mongodb'
+import muid from 'uuid-mongodb'
 import { ContextWithAuth } from '../../types.js'
 
 const ClimbMutations = {
-  addClimbs: async (_, { input }, { dataSources, user }: ContextWithAuth): Promise<string[] | null> => {
+  addClimbs: async (_, { input }, { dataSources, user }: ContextWithAuth): Promise<string[]> => {
     const { climbs: ds } = dataSources
     const { climbs, parentId } = input
 
-    // permission middleware shouldn't send undefined uuid
-    // Temporarily disable Auth check
-    // if (user?.uuid == null) throw new Error('Missing user uuid')
+    if (user?.uuid == null) throw new Error('Missing user uuid')
 
-    const uidList = await ds.addClimbs(muid.v4(), muid.from(parentId), climbs)
-    return uidList?.map(id => id.toUUID().toString()) ?? null
+    return await ds.addClimbs(user.uuid, muid.from(parentId), climbs)
   },
 
-  updateClimbs: async (_, { input }, { dataSources, user }: ContextWithAuth): Promise<MUUID[]> => {
+  updateClimbs: async (_, { input }, { dataSources, user }: ContextWithAuth): Promise<string[]> => {
     const { climbs: ds } = dataSources
     const { changes, parentId } = input
-    // if (user?.uuid == null) throw new Error('Missing user uuid')
-    // return await ds.deleteClimbs(muid.v4(), input as)
 
-    return await ds.updateClimbs(muid.v4(), muid.from(parentId), changes)
+    if (user?.uuid == null) throw new Error('Missing user uuid')
+
+    return await ds.updateClimbs(user.uuid, muid.from(parentId), changes)
   },
 
   deleteClimbs: async (_, { idList }, { dataSources, user }: ContextWithAuth): Promise<number> => {
     const { climbs: ds } = dataSources
 
-    // if (user?.uuid == null) throw new Error('Missing user uuid')
-    return await ds.deleteClimbs(muid.v4(), idList as string[])
+    if (user?.uuid == null) throw new Error('Missing user uuid')
+
+    return await ds.deleteClimbs(user.uuid, idList as string[])
   }
 }
 
