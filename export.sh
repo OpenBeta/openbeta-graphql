5bec9da54b853500fe079a2af5ab312a897f513b
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2023-02-10 10:40:26 +0100

    fix(export): git clone requires a dummy user name and token (#228)
    Co-authored-by: viet nguyen <vietnguyen@noreply>

--- export.sh
+++ export.sh
@@ -2,12 +2,12 @@
 
 if [ -z ${GITHUB_ACCESS_TOKEN} ]
 then
-  echo "GITHUB_ACCESS_TOKEN var not defined."
+  echo "GITHUB_ACCESS_TOKEN not defined."
   exit 1
 fi
 
 echo "cloning openbeta-export repository"
-git clone --depth 1 --branch production https://${GITHUB_ACCESS_TOKEN}@github.com/OpenBeta/openbeta-export || exit 1
+git clone --depth 1 --branch production https://ob-bot-user:${GITHUB_ACCESS_TOKEN}@github.com/OpenBeta/openbeta-export || exit 1
 cd openbeta-export
 git config user.name "db-export-bot"
 git config user.email "db-export-bot@noreply"
@@ -19,7 +19,6 @@ yarn export:json:full --output openbeta-export
 echo "... finished export. Committing data..."
 
 cd openbeta-export || exit 1
-
 git add -A
 git commit -am "export openbeta data"
 git push origin production
