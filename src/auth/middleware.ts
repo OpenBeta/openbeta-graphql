28e490ceef246417911e0761d264b5dbcba1483a
CocoisBuggy <g.colinharley@gmail.com>
2023-02-17 15:53:49 +0200

    Repaired some floating types
    Should be a little harder to break in the future now.
    Question for future devs: How are we supposed to handle verifiedToken
    actually NOT being valid? I imagine an error is thrown, but the types
    also indicate a string (Rather than an object) may be returned in normal
    operation.

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -1,11 +1,16 @@
 import muid from 'uuid-mongodb'
 import { AuthUserType } from '../types.js'
 import { verifyJWT } from './util.js'
+import { ExpressContext } from 'apollo-server-express/dist/ApolloServer'
 
 /**
- * Create a middleware context for Apollo server
+ * Create a middleware context for Apollo server.
+ * The primary purpose of this context creator is to take the Auth bearer
+ * token and decode it to get the current requests user claim.
+ *
+ * We are implicityly trusting all claims made by the JWT.
  */
-export const createContext = async ({ req }): Promise<any> => {
+export async function createContext ({ req }: ExpressContext): Promise<{user: AuthUserType}> {
   const { headers } = req
 
   const user: AuthUserType = {
@@ -14,12 +19,13 @@ export const createContext = async ({ req }): Promise<any> => {
   }
 
   const authHeader = String(headers?.authorization ?? '')
+
   if (authHeader.startsWith('Bearer ')) {
     const token = authHeader.substring(7, authHeader.length).trim()
-    const z = await verifyJWT(token)
+    const verifiedToken = await verifyJWT(token)
 
-    user.roles = z?.['https://tacos.openbeta.io/roles'] ?? []
-    const uidStr: string | undefined = z?.['https://tacos.openbeta.io/uuid']
+    user.roles = verifiedToken?.['https://tacos.openbeta.io/roles'] ?? []
+    const uidStr: string | undefined = verifiedToken?.['https://tacos.openbeta.io/uuid']
     user.uuid = uidStr != null ? muid.from(uidStr) : undefined
   }
 
