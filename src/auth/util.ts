494292635d3d46374bc107b21c626cbd54378070
CocoisBuggy <g.colinharley@gmail.com>
2023-02-22 19:47:38 +0200

    Type corrections. Removed explicit 'any'

--- src/auth/util.ts
+++ src/auth/util.ts
@@ -1,5 +1,5 @@
 import jwksClient from 'jwks-rsa'
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
 import { checkVar } from '../db/index.js'
 
@@ -10,7 +10,12 @@ const client = jwksClient({
   jwksUri: `${auth0Domain}/.well-known/jwks.json`
 })
 
-export const verifyJWT = async (token): Promise<any> => {
+/**
+ * Synchronously verify given token using a secret or a public key to get a decoded token
+ * token - JWT string to verify
+ * returns - The decoded token.
+ * */
+export const verifyJWT = async (token): Promise<JwtPayload | string> => {
   const key = await client.getSigningKey(auth0Kid)
   return jwt.verify(token, key.getPublicKey())
 }
