da6d53e99d5ef767f4a587303a5609545985ad07
CocoisBuggy <g.colinharley@gmail.com>
2023-02-22 19:48:12 +0200

    Wrapped into a main function

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,10 @@
-import { ApolloServer } from 'apollo-server'
-import mongoose from 'mongoose'
-import { applyMiddleware } from 'graphql-middleware'
-import { graphqlSchema } from './graphql/resolvers.js'
 
 import { connectDB, defaultPostConnect } from './db/index.js'
-import { createInstance as createNewAreaDS } from './model/MutableAreaDataSource.js'
-import { changelogDataSource } from './model/ChangeLogDataSource.js'
-import MutableMediaDataSource from './model/MutableMediaDataSource.js'
-import { createInstance as createNewClimbDS } from './model/MutableClimbDataSource.js'
-import TickDataSource from './model/TickDataSource.js'
-import { createContext, permissions } from './auth/index.js'
 import { logger } from './logger.js'
+import { getGqlServer } from './graphql/server.js'
 
-// eslint-disable-next-line
-(async function (): Promise<void> {
-  const schema = applyMiddleware(graphqlSchema, permissions.generate(graphqlSchema))
-  const server = new ApolloServer({
-    introspection: true,
-    schema,
-    context: createContext,
-    dataSources: () => ({
-      climbs: createNewClimbDS(),
-      areas: createNewAreaDS(),
-      ticks: new TickDataSource(mongoose.connection.db.collection('ticks')),
-      history: changelogDataSource, // see source for explantion why we don't instantiate the object
-      media: new MutableMediaDataSource(mongoose.connection.db.collection('media'))
-    }),
-    cache: 'bounded'
-  })
+async function main (): Promise<void> {
+  const server = await getGqlServer()
 
   await connectDB(defaultPostConnect)
 
@@ -38,4 +15,9 @@ import { logger } from './logger.js'
   }).then((): void => {
     logger.info(`🚀 Server ready at http://localhost:${port}`)
   })
-})()
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
