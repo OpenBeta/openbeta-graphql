a30496558cf4891d7280635487f53a3f05b32d1a
CocoisBuggy <g.colinharley@gmail.com>
2023-02-28 19:44:20 +0200

    Migration to ApolloServer V4
    Apollo Server 3 is officially deprecated, with end-of-life scheduled for
    22 October 2023.
    
    If you skim through apollographql.com/docs/apollo-server/migration this
    is pretty much the result you get.
    
    With some type repair in middleware.ts I can be a little more confident,
    but since we have no integration testing for the GQL layer it's hard to
    know if the API is still producing the same Data.

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -1,11 +1,16 @@
 import muid from 'uuid-mongodb'
 import { AuthUserType } from '../types.js'
 import { verifyJWT } from './util.js'
+import { StandaloneServerContextFunctionArgument as ContextInput } from '@apollo/server/dist/esm/standalone/index.js'
+
+export interface GraphQLContext {
+  user: AuthUserType
+}
 
 /**
  * Create a middleware context for Apollo server
  */
-export const createContext = async ({ req }): Promise<any> => {
+export async function createContext ({ req }: ContextInput): Promise<GraphQLContext> {
   const { headers } = req
 
   const user: AuthUserType = {
@@ -14,6 +19,7 @@ export const createContext = async ({ req }): Promise<any> => {
   }
 
   const authHeader = String(headers?.authorization ?? '')
+
   if (authHeader.startsWith('Bearer ')) {
     const token = authHeader.substring(7, authHeader.length).trim()
     const z = await verifyJWT(token)
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,33 @@
-import { ApolloServer } from 'apollo-server'
-import mongoose from 'mongoose'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
 import { applyMiddleware } from 'graphql-middleware'
 import { graphqlSchema } from './graphql/resolvers.js'
 
 import { connectDB, defaultPostConnect } from './db/index.js'
-import { createInstance as createNewAreaDS } from './model/MutableAreaDataSource.js'
-import { changelogDataSource } from './model/ChangeLogDataSource.js'
-import MutableMediaDataSource from './model/MutableMediaDataSource.js'
-import { createInstance as createNewClimbDS } from './model/MutableClimbDataSource.js'
-import TickDataSource from './model/TickDataSource.js'
 import { createContext, permissions } from './auth/index.js'
 import { logger } from './logger.js'
+import { GraphQLContext } from './auth/middleware.js'
 
-// eslint-disable-next-line
-(async function (): Promise<void> {
+async function main (): Promise<void> {
   const schema = applyMiddleware(graphqlSchema, permissions.generate(graphqlSchema))
-  const server = new ApolloServer({
-    introspection: true,
-    schema,
-    context: createContext,
-    dataSources: () => ({
-      climbs: createNewClimbDS(),
-      areas: createNewAreaDS(),
-      ticks: new TickDataSource(mongoose.connection.db.collection('ticks')),
-      history: changelogDataSource, // see source for explantion why we don't instantiate the object
-      media: new MutableMediaDataSource(mongoose.connection.db.collection('media'))
-    }),
-    cache: 'bounded'
-  })
+  const server = new ApolloServer<GraphQLContext>({ schema })
 
   await connectDB(defaultPostConnect)
 
-  const port = 4000
-
-  await server.listen({
-    port
-  }).then((): void => {
-    logger.info(`🚀 Server ready at http://localhost:${port}`)
+  const { url } = await startStandaloneServer(server, {
+    context: createContext,
+    listen: { port: 4000 }
   })
-})()
+
+  if (typeof url !== 'string') {
+    throw new Error('Server did not appear to start correctly')
+  }
+
+  logger.info(`🚀  Server ready at ${url}`)
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
