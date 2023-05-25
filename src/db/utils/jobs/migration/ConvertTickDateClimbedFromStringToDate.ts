714d83a14f119a69bdb710029b1cad629578de6e
kao <zichongkao@gmail.com>
2023-05-24 18:11:57 -0600

    Convert Tick dateClimber from string to date

---
+++ src/db/utils/jobs/migration/ConvertTickDateClimbedFromStringToDate.ts
@@ -0,0 +1,30 @@
+import { connectDB } from '../../../index.js'
+import { logger } from '../../../../logger.js'
+import { getTickModel } from '../../../TickSchema.js'
+
+/**
+ * Converts dateClimbed field on ticks from string to date.
+ */
+const onConnected = async (): Promise<void> => {
+  logger.info('Migrating ticks...')
+  const tickModel = getTickModel()
+
+  await (await tickModel.updateMany(
+    { dateClimbed: { $exists: true } },
+    {
+      $set: {
+        dateClimbed: {
+          $dateFromString: {
+            dateString: '$dateClimbed',
+            // We want to ascribe an hour of day to the climb, so it shows
+            // up on the correct day when displayed in the user's timezone.
+            // Most climbs are in the US, MT time is a good first approximation.
+            timezone: 'America/Denver'
+          }
+        }
+      }
+    }
+  ))
+}
+
+void connectDB(onConnected)
