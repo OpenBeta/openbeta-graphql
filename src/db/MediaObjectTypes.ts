35653b586b2f7b04d20227616578935ff0824c1d
viet nguyen <vietnguyen@noreply>
2023-07-12 10:48:22 -0700

    refactor: simplify add media api to accept fewerparams for embedded tag
    refactor: seed-db script should only download db file once

--- src/db/MediaObjectTypes.ts
+++ src/db/MediaObjectTypes.ts
@@ -13,7 +13,7 @@ export interface MediaObject {
   format: ImageFormatType
   createdAt: Date
   size: number
-  entityTags: EntityTag[]
+  entityTags?: EntityTag[]
 }
 
 export interface EntityTag {
@@ -52,6 +52,11 @@ export interface TagsLeaderboardType {
   allTime: AllTimeTagStats
 }
 
+/**
+ * For creating a new Media object doc
+ */
+export type NewMediaObjectDoc = Omit<MediaObject, '_id' | 'createdAt'>
+
 export interface UserMediaGQLQueryInput {
   userUuid: string
   maxFiles?: number
@@ -84,7 +89,7 @@ export interface EntityTagDeleteInput {
  */
 export type MediaObjectGQLInput = Pick<MediaObject, 'mediaUrl' | 'width' | 'height' | 'format' | 'size'> & {
   userUuid: string
-  entityTags?: Array<Omit<AddEntityTagGQLInput, 'mediaId'>>
+  entityTag?: Omit<AddEntityTagGQLInput, 'mediaId'>
 }
 
 /**
