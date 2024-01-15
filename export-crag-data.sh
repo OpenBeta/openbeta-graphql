e0966c0cd11169573e7117bb84397b8a7a6764c9
viet nguyen <vietnguyen@noreply>
2024-01-14 18:26:11 -0800

    feat: update github push script

--- export-crag-data.sh
+++ export-crag-data.sh
@@ -6,16 +6,23 @@ then
   exit 1
 fi
 
-echo "cloning openbeta-export repository"
-git clone --depth 1 --branch production https://ob-bot-user:${GITHUB_ACCESS_TOKEN}@github.com/OpenBeta/openbeta-export || exit 1
-git config user.name "db-export-bot"
-git config user.email "db-export-bot@noreply"
-cd ..
-
 echo "start exporting CRAG data..."
 yarn export-crags
 
+ZIP_FILE=crags.geojson.gz
+
+tar -czf ${ZIP_FILE} crags.geojson crag-groups.geojson
+
 echo "... finished export. Committing data..."
+
+echo "cloning openbeta-export repository"
+git clone --depth 1 --branch production https://ob-export-user:${GITHUB_ACCESS_TOKEN}@github.com/OpenBeta/openbeta-export.git || exit 1
+
+cd openbeta-export
+git config user.name "ob-export-user"
+git config user.email "db-export-bot@noreply"
+
+mv ../${ZIP_FILE} .
 
 git add -A
 git commit -am "export crag data"
