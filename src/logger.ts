e2aeba1973d4210d5ad1fc88768b9e64b9566cf4
Iacami Gevaerd <enapupe@gmail.com>
2024-01-19 15:17:06 -0300

    chore: forward app logs to logflare for better vis

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,18 @@
 import pino from 'pino'
+import { createWriteStream } from 'pino-logflare'
+
+const setupLogFlare = (apiKey?: string, sourceToken?: string): any | undefined => {
+  if (typeof apiKey !== 'undefined' && typeof sourceToken !== 'undefined') {
+    return createWriteStream({
+      apiKey,
+      sourceToken
+    })
+  }
+  return undefined
+}
 
 export const logger = pino({
+  stream: setupLogFlare(process.env.LOGFLARE_API_KEY, process.env.LOGFLARE_SOURCE_TOKEN),
   name: 'openbeta-graphql',
   level: 'info'
 })
