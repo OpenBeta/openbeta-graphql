548aecf2708d26c7650dd4077a93d518a18f30b7
viet nguyen <vietnguyen@noreply>
2024-02-24 09:09:13 -0800

    refactor: clean up unused pkgs and files

--- scripts/upload-tiles.sh
+++ scripts/upload-tiles.sh
@@ -6,19 +6,22 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 set -a
-. ${SCRIPT_DIR}/../.env
-. ${SCRIPT_DIR}/../.env.local
+. ${SCRIPT_DIR}/../.env 2> /dev/null
+. ${SCRIPT_DIR}/../.env.local 2> /dev/null
 set +a
 
+# Define Cloudflare-R2 backend for rclone
 S3_DEST=':s3,provider=Cloudflare,no_check_bucket=true,env_auth=true,acl=private:maptiles'
 
-echo "Generating crags tiles file"
-#tippecanoe --force -o ${MAPTILES_WORKING_DIR}/crags.pmtiles -l crags -n "Crags" -zg ${MAPTILES_WORKING_DIR}/crags.*.geojson
+echo "------ Generating crags tiles file ------"
+tippecanoe --force -o ${MAPTILES_WORKING_DIR}/crags.pmtiles -l crags -n "Crags" -zg ${MAPTILES_WORKING_DIR}/crags.*.geojson
+
 echo "**Uploading to remote storage"
 rclone copy ${MAPTILES_WORKING_DIR}/crags.pmtiles ${S3_DEST}
 
-echo "Generating crag group tiles file"
-#tippecanoe --force -o ${MAPTILES_WORKING_DIR}/crag-groups.pmtiles -l crag-groups -n "Crag groups" -zg ${MAPTILES_WORKING_DIR}/crag-groups.geojson
+echo "------ Generating crag group tiles file ------"
+tippecanoe --force -o ${MAPTILES_WORKING_DIR}/crag-groups.pmtiles -l crag-groups -n "Crag groups" -zg ${MAPTILES_WORKING_DIR}/crag-groups.geojson
+
 echo "**Uploading to remote storage"
 rclone copy ${MAPTILES_WORKING_DIR}/crag-groups.pmtiles ${S3_DEST}
 
