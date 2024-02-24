5db067b8650960a8b81190bc29044f23ae4b2b31
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2024-02-24 11:43:36 -0700

    fix: remove unused mapbox var checks (#395)
    Co-authored-by: viet nguyen <vietnguyen@noreply>

--- src/db/utils/jobs/MapTiles/init.ts
+++ src/db/utils/jobs/MapTiles/init.ts
@@ -2,20 +2,10 @@ import { config } from 'dotenv'
 
 config({ path: ['.env.local', '.env'] })
 
-const mapboxUsername = process.env.MAPBOX_USERNAME ?? ''
-const mapboxToken = process.env.MAPBOX_TOKEN ?? ''
 const workingDir = process.env.MAPTILES_WORKING_DIR ?? ''
-
-if (mapboxUsername.trim() === '') {
-  throw new Error('MAPBOX_USERNAME not set')
-}
-
-if (mapboxToken.trim() === '') {
-  throw new Error('MAPBOX_TOKEN not set')
-}
 
 if (workingDir.trim() === '') {
   throw new Error('MAPTILES_WORKING_DIR not set')
 }
 
-export { mapboxUsername, mapboxToken, workingDir }
+export { workingDir }
