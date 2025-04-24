c250fb9bb2e3f10ac2cc241db4aa964109b5fbf0
CocoisBuggy <g.colinharley@gmail.com>
2025-04-24 14:14:08 +0200

    Force vite to only collect test files (by name)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts', 'src/**/__tests__/**/*.ts'],
-    exclude: ['**/*/fixtures', '**/*/setup.ts'],
+    include: ['src/**/*.test.ts'],
     globals: true,
     globalSetup: './src/__tests__/setup.ts',
     environment: 'node',
