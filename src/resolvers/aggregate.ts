c9f3a2482d928fd08846dda3a190f439ca48def5
CocoisBuggy <g.colinharley@gmail.com>
2026-02-05 19:45:43 +0200

    demo

--- src/resolvers/aggregate.ts
+++ src/resolvers/aggregate.ts
@@ -2,12 +2,18 @@ import { Resolvers } from '@gql';
 
 export const aggregateTypeResolvers: Resolvers['AggregateType'] = {
   byGrade: async (parent) => {
-    throw new Error('Not implemented');
+    // TODO
+    // throw new Error('Not implemented');
+    return [];
   },
   byDiscipline: async (parent) => {
-    throw new Error('Not implemented');
+    // TODO
+    // throw new Error('Not implemented');
+    return {};
   },
   byGradeBand: async (parent) => {
-    throw new Error('Not implemented');
+    // TODO
+    // throw new Error('Not implemented');
+    return {};
   },
 };
